eda657322343999e7add9081df9413bec8954833
popejb1116 <popejb1116@gmail.com>
2019-05-21 12:54:38 -0400

    Deleted and removed TextInfoContext references

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-import { TextInfoProvider } from './context/TextInfoContext'
+
 import { PlaceholderProvider } from './context/PlaceholderContext'
 import { theme } from './theme/Theme'
 import { ThemeProvider } from 'styled-components'
 
 ReactDOM.render(
 <ThemeProvider theme={theme}>
-<TextInfoProvider>
 <PlaceholderProvider>
    <App />
 </PlaceholderProvider>
-</TextInfoProvider>
 </ThemeProvider>
-, document.getElementById('root'));
+, document.getElementById('root'))
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
